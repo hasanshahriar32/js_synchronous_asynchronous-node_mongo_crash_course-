f7fa7eb6f82aa7ff550b7babec97de97bb6c8274
Mr. Hasan <hasanshahriar32@gmail.com>
2023-04-24 21:04:27 +0600

    update d async promise

--- promise.js
+++ promise.js
@@ -5,9 +5,22 @@ const myPromise = new Promise((resolve, reject) => {
     return;
   }
   setTimeout(() => {
-    resolve("successfully got the data");
+    resolve({ name: "john" });
   }, 1000);
 });
+
+const userIds = [1, 5, 9, 8, 9];
+let userData = [];
+
+for (let i = 0; i < userIds.length; i++) {
+  const userId = userIds[i];
+  userData.push(myPromise);
+}
+
+Promise.all(userData).then((res) => {
+  console.log(res);
+});
+// console.log(userData);
 
 myPromise
   .then((res) => {
